a1211018ce1d42c3d0e13881c79a6e2ac72dd3ea
yinxaunzhang <a15346788558@gmail.com>
2026-03-22 15:17:01 -0700

    Make progress on homepage CSS

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -28,5 +28,46 @@ export function HomePage(){
   </div>
   </div>
   </div>
+
+  <div className='middle-section-banner'>
+    <div className='leftPicture'><img src="/images/banner-left.png" alt="" />
+    <p>手工制作</p></div>
+    <div className='rightPicture'><img src="/images/banner-right.png" alt="" />
+    <p>新鲜出炉</p>
+    </div>
+  </div>
+
+  <div className='products-Gride'>
+    <div className="product">
+      <img src="/images/resources/cellImage_0_0.jpg" alt="" />
+    </div>
+    <div className="product">
+      <img src="/images/resources/cellImage_0_1.jpg" alt="" />
+    </div>
+    <div className="product">
+      <img src="/images/resources/cellImage_0_2.jpg" alt="" />
+    </div>
+    <div className="product">
+      <img src="/images/resources/cellImage_0_3.jpg" alt="" />
+    </div>
+    <div className="product">
+      <img src="/images/resources/cellImage_0_6.jpg" alt="" />
+    </div>
+    <div className="product">
+      <img src="/images/resources/cellImage_0_7.jpg" alt="" />
+    </div>
+    <div className="product">
+      <img src="/images/resources/cellImage_0_8.jpg" alt="" />
+    </div>
+    <div className="product">
+      <img src="/images/resources/cellImage_0_9.jpg" alt="" />
+    </div>
+    <div className="product">
+      <img src="/images/resources/cellImage_0_17.jpg" alt="" />
+    </div>
+    <div className="product">
+      <img src="/images/resources/cellImage_0_11.jpg" alt="" />
+    </div>
+  </div>
 </>);
 }
