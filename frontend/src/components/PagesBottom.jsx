8e0bf0afe23f746452d6eacdd5b2bdc8b4748a04
yinxaunzhang <a15346788558@gmail.com>
2026-06-04 21:41:20 -0700

    update owner information

--- frontend/src/components/PagesBottom.jsx
+++ frontend/src/components/PagesBottom.jsx
@@ -7,15 +7,15 @@ export function PagesBottom() {
         <div className="footer-column about-us">
           <div className="footer-header">About Us</div>
           <div className="footer-single-information">Address: 7288 188 Street, Surrey, BC</div>
-          <div className="footer-single-information">Email: 1789893950@qq.com</div>
+          <div className="footer-single-information">Email: linahappy0709@gmail.com</div>
           <div className="footer-single-information">Owner: Lina Ma</div>
         </div>
 
         <div className="footer-column contact-with-us">
           <div className="footer-header">Contact Us</div>
           <div className="footer-single-information">Business Hours: 7:00 - 21:30</div>
-          <div className="footer-single-information">Phone: 778-513-3120</div>
-          <div className="footer-single-information">Email: 1789893950@qq.com</div>
+          <div className="footer-single-information">Phone: 778-865-6707</div>
+          <div className="footer-single-information">Email: linahappy0709@gmail.com</div>
           <div className="footer-single-information">
             <a href="https://www.instagram.com/lmlina929/" target="_blank" rel="noreferrer">
               Instagram
